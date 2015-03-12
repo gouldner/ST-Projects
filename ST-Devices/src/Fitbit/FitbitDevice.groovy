2925ad5ac388eb1c56e4d8e88be5902a9bd6b7d5
Ronald Gouldner <gouldner@hawaii.edu>
2015-03-11 16:46:17 -1000

    Update FitbitDevice.groovy

--- ST-Devices/src/Fitbit/FitbitDevice.groovy
+++ ST-Devices/src/Fitbit/FitbitDevice.groovy
@@ -13,10 +13,13 @@
  *  for the specific language governing permissions and limitations under the License.
  *
  */
- 
+
 preferences {
-    input("user", "text", title: "Fitbut User")
-    
+    input("user", "text", title: "Fitbit User")
+    input("fitbit_auth_pin", "text", title: "Fitbit Access Auth Pin")
+    input("fitbit_auth_token", "text", title: "Fitbit oauth token")
+    input("fitbit_auth_token_secret", "text", title: "Fitbit oauth token secret")
+    input("fitbit_dev_smarthings_app_client_consumer_key", "text", title: "Fitbit registered app client consumer key")
 }
 metadata {
 	definition (name: "Fitbit Device", namespace: "gouldner", author: "Ronald Gouldner") {
@@ -65,13 +68,103 @@ def parse(String description) {
 }
 
 def poll() {
-	refresh()
+	//refresh()
+}
+
+
+
+
+def OAuthGet() {
+
+/* 
+   code found here https://code.google.com/p/oauth-signpost/wiki/TwitterAndSignpost
+*/ 
+
+/*
+	    OAuthConsumer consumer = new DefaultOAuthConsumer(
+                // the consumer key of this app (replace this with yours)
+                "iIlNngv1KdV6XzNYkoLA",
+                // the consumer secret of this app (replace this with yours)
+                "exQ94pBpLXFcyttvLoxU2nrktThrlsj580zjYzmoM");
+
+        OAuthProvider provider = new DefaultOAuthProvider(
+                "http://twitter.com/oauth/request_token",
+                "http://twitter.com/oauth/access_token",
+                "http://twitter.com/oauth/authorize");
+ */               
+        /****************************************************
+         * The following steps should only be performed ONCE
+         ***************************************************/
+/*
+        // we do not support callbacks, thus pass OOB
+        String authUrl = provider.retrieveRequestToken(consumer, OAuth.OUT_OF_BAND);
+
+        // bring the user to authUrl, e.g. open a web browser and note the PIN code
+        // ...         
+
+        String pinCode = // ... you have to ask this from the user, or obtain it
+        // from the callback if you didn't do an out of band request
+
+        // user must have granted authorization at this point
+        provider.retrieveAccessToken(consumer, pinCode);
+
+        // store consumer.getToken() and consumer.getTokenSecret(),
+        // for the current user, e.g. in a relational database
+        // or a flat file
+        // ...
+*/
+        /****************************************************
+         * The following steps are performed everytime you
+         * send a request accessing a resource on Twitter
+         ***************************************************/
+/*
+        // if not yet done, load the token and token secret for
+        // the current user and set them
+        consumer.setTokenWithSecret(ACCESS_TOKEN, TOKEN_SECRET);
+
+        // create a request that requires authentication
+        URL url = new URL("http://twitter.com/statuses/mentions.xml");
+        HttpURLConnection request = (HttpURLConnection) url.openConnection();
+
+        // sign the request
+        consumer.sign(request);
+
+        // send the request
+        request.connect();
+
+        // response status should be 200 OK
+        int statusCode = request.getResponseCode();
+        */
 }
 
 def refresh() { 
+
   log.debug "Executing 'refresh'"
   
-  def cmd = "https://api.fitbit.com/1/${settings.user}/-/body/log/weight/date/2015-03-05.json";
+  def fetchDate="2015-03-05"
+  def oauthNonce=(int) (Math.random() * 100000000)
+  def now=new Date()
+  def timeStamp=now.toTimestamp()
+  
+  /* Get Sample
+  def pollParams = [
+    uri: "https://api.ecobee.com",
+    path: "/1/thermostat",
+    headers: ["Content-Type": "text/json", "Authorization": "Bearer ${atomicState.authToken}"],
+    query: [format: 'json', body: jsonRequestBody]
+    
+    */
+  
+  /*
+  def bodyDataPollParams = [
+    uri: "https//api.fitbit.com",
+    path: "/1/user/${settings.user}/body/date/${fetchDate}.json",
+    headers: ["Content-Type": "text/json", "Authorization: OAuth oauth_consumer_key="${settings.fitbit_dev_smarthings_app_client_consumer_key}", oauth_nonce="${oauthNonce}", oauth_signature="1gqjxxh1dWGExCe8NH%2Fo%2BTfadOk%3D", oauth_signature_method="HMAC-SHA1", oauth_timestamp="${timeStamp}", oauth_token="${settings.fitbit_auth_token}", oauth_version="1.0"],
+    query: [format: 'json', body: jsonRequestBody]
+    
+    */
+  
+  def cmd = "https//api.fitbit.com/1/user/${settings.user}/body/date/${fetchDate}.json&oauth_consumer_key=${settings.fitbit_dev_smarthings_app_client_consumer_key}&oauth_nonce=${randomText}&oauth_signature_method=HMAC-SHA1&oauth_timestamp=${timeStamp}&oauth_token=${settings.fitbit_auth_token}&oauth_version=1.0";
   log.debug "Sending request cmd[${cmd}]"
   
   httpGet(cmd) {resp ->
